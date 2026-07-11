486dde1c9054e8f9d8c71a9f0b53d927c216668a
sohaib271 <sohaibsheikh6299@gmail.com>
2026-07-11 18:33:35 +0500

    fewg

--- .vercel/output/functions/__server.func/_ssr/checkup.service-CQreqd1m.mjs
+++
@@ -1,9 +0,0 @@
-import { r as api } from "./api-CC38_k8-.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/checkup.service-CQreqd1m.js
-var CheckupService = class {
-	static async findOrCreate(data) {
-		return (await api.post("/checkups/find-or-create", data)).data;
-	}
-};
-//#endregion
-export { CheckupService as t };
--- .vercel/output/static/assets/checkup.service-D5DkMVsf.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./dist-t4Ar18J9.js";var t=class{static async findOrCreate(t){return(await e.post(`/checkups/find-or-create`,t)).data}};export{t};
